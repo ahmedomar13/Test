3315f69f20d914b0a550130f0cc5ba66c914f9cd
Your Name <you@example.com>
2019-10-28 21:44:43 +0200

    first commit

---
+++ bin/test.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+void main() {
+  /*  print('Hello, World!');
+ var user = 'Bill';
+  var city = 'Cairo';
+  print("Hello $user . are you $city");
+  print('${1+2}');
+  var age =18;
+  if(age>=18)
+    print("you can vote");
+       else
+    print("you can not vote");
+  for(int i=0;i<4;i++){
+    print(i);
+  }
+  var cites=['cairo','alex','mansoure'];
+  for(var city in cites){
+    print(city);
+  }
+
+  var num1=stdin.readLineSync();
+  var num2=stdin.readLineSync();
+  var operator = stdin.readLineSync();
+
+  var number1=double.parse(num1);
+  var number2=double.parse(num2);
+var res=0.0;
+
+
+  switch(operator){
+    case'+':
+      res=number1+number2;
+      print(res);
+      break;
+    case'-':
+      res=number1-number2;
+      print(res);
+      break;
+      case'*':
+        res=number1*number2;
+    print(res);
+    break;
+    case'/':
+      res=number1/number2;
+      print(res);
+      break;
+
+  }
+
+  //var emp=new Employee();
+  //emp.fristname='Ahmed';
+  //emp.lastname='omar';
+  //emp.age=26;
+  //emp.salary=5000;
+ // print(emp.fristname  +' ' + emp.lastname);
+
+
+Employee employee= new Employee();
+
+employee.PrintName((String fristname){
+    print(fristname);
+  }
+  );
+//int
+  var emp=new add();
+print(emp(20,10));*/
+
+  var emp = new Employee();
+
+  }
+
+class Employee{
+var name;
+var age;
+var salary;
+
+  /*  int add(int num1,{int num2 =5}){
+    return num1+num2;
+  }
+
+String fristname;
+  String lastname;
+  int age;
+  double salary;
+
+   PrintName(Function f){
+     String fristname='Ahmed';
+     f(fristname);
+   }*/
+
+}
+
+
+
